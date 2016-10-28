d449f6801fa1da28747a119072c4f436eb22b270
Tim You <tyou0927606@yahoo.com>
2016-10-28 09:50:20 -0700

    module1 solution submission

---
+++ module1-solution/app.js
@@ -0,0 +1,31 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheckApp', [])
+.controller('LunchCheckController', LunchCheckController);
+
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+  $scope.lunch_items = "";
+  $scope.evaluation_result = "";
+
+  $scope.checkLunch = function () {
+    var lunch_items = $scope.lunch_items;
+    var lunch_items_count = lunch_items.split(",").filter(String).filter(function(e){return e != '""'}).length;
+
+    var evaluation_result;
+    if (lunch_items_count == 0 ){
+      evaluation_result = "Please enter data first, empty text between commas not counted!";
+    } else if (lunch_items_count > 3 ) {
+      evaluation_result = "Too Much!";
+    } else if (lunch_items_count >= 1  && lunch_items_count <= 3){
+      evaluation_result = "Enjoy!";
+    }
+
+    $scope.evaluation_result = evaluation_result;
+  };
+
+}
+
+})();
